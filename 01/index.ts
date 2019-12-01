ba2830a03ffecb8f1ff68736bba3d9250cea0f45
Steven Musumeche <steven@musumeche.com>
2019-12-01 13:32:28 -0600

    day 1 part 1 solution

--- 01/index.ts
+++ 01/index.ts
@@ -1,3 +1,24 @@
-console.log('hello world');
+import fs from 'fs';
 
-console.log('hello world 2')
+/**
+ * To find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+ */
+const calcFuel = (mass: number): number => {
+  return Math.floor(mass / 3) - 2
+}
+
+const getInput = (): number[] => {
+  const input = fs.readFileSync(__dirname + '/input1.txt');
+  return input.toString().split("\n").map(Number);
+}
+
+(async function main() {
+  const input = getInput();
+  
+  const sum = input.reduce((acc, cur) => {
+    acc += calcFuel(cur);
+    return acc;
+  }, 0)
+
+  console.log('Answer', sum);
+})();
